24023a23c4ca68f2e4334104209e4d3a286f3b44
tonijigan <100856397+tonijigan@users.noreply.github.com>
2022-12-01 19:24:28 +0300

    init

--- Assets/Scripts/EnemyMovment.cs
+++ Assets/Scripts/EnemyMovment.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyMovment : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemy;
+    [SerializeField] private EnemyMovment _enemy;
     [SerializeField] private Transform _spawnPoints;
     [SerializeField] private float _numberOfSeconds;
 
